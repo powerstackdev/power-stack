c47c3ee47980c2d365936c084cb14bdddc58d356
Xavier Mirabelli-Montan <xavier.mm@investisdigital.com>
2022-07-12 12:59:10 +0100

    Adding to the docs

---
+++ docs/src/pages/blog.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const BlogPage = () => {
+  return (
+    <main>
+      <title>Blog</title>
+      <h1 >Blog listing page to go here...</h1>
+      <p>...eventually!</p>
+    </main>
+  );
+};
+
+export default BlogPage;
