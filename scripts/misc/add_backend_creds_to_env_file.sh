539d6d4578a7b3a82476889c2e8c61f894c4b456
Xavier Mirabelli-Montan <xavier.mm@investisdigital.com>
2022-11-30 09:05:52 +0000

    Upgrading and fixing issues caused by upgrade of simple oauth

--- scripts/misc/add_backend_creds_to_env_file.sh
+++ scripts/misc/add_backend_creds_to_env_file.sh
@@ -3,5 +3,5 @@
 
 cd "${PROJECT_ROOT}/backend/starters/${BACKEND_STARTER}"
 
-update_env_file "GATSBY_CLIENT_ID" $(drush eval "echo \Drupal::entityTypeManager()->getStorage('consumer')->load(2)->uuid->value;")
+update_env_file "GATSBY_CLIENT_ID" $(drush eval "echo \Drupal::entityTypeManager()->getStorage('consumer')->load(2)->client_id->value;")
 update_env_file "GATSBY_DRUPAL_KEY" $(drush eval "echo \Drupal::entityTypeManager()->getStorage('user')->load(2)->api_key->value;")
